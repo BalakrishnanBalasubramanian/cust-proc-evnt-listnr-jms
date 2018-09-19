a89ecfa2a9afe41b56268919e2b46342931554f2
bbalasub <bbalasub@localhost.localdomain>
2018-09-19 11:09:30 +0800

    Added custom implementation of event listeners

---
+++ src/main/java/com/bala/custom/listener/SelectedEventListener.java
@@ -0,0 +1,45 @@
+package com.bala.custom.listener;
+
+import org.jbpm.process.audit.jms.AsyncAuditLogProducer;
+import org.kie.api.event.process.ProcessCompletedEvent;
+import org.kie.api.event.process.ProcessStartedEvent;
+import org.kie.api.event.process.ProcessVariableChangedEvent;
+
+public class SelectedEventListener extends AsyncAuditLogProducer {
+
+	public void beforeProcessStarted(ProcessStartedEvent event) {
+		// Do nothing
+		System.out.println("Inside beforeProcessStarted event");
+	}
+
+	@Override
+	public void afterProcessStarted(ProcessStartedEvent event) {
+		// Do nothing
+		System.out.println("Inside afterProcessStarted event");
+	}
+
+	@Override
+	public void beforeProcessCompleted(ProcessCompletedEvent event) {
+		// Do nothing
+		System.out.println("Inside beforeProcessCompleted event");
+	}
+
+	@Override
+	public void afterProcessCompleted(ProcessCompletedEvent event) {
+		// Do nothing
+		System.out.println("Inside afterProcessCompleted event");
+	}
+
+	@Override
+	public void beforeVariableChanged(ProcessVariableChangedEvent event) {
+		// Do nothing
+		System.out.println("Inside beforeVariableChanged event");
+	}
+
+	@Override
+	public void afterVariableChanged(ProcessVariableChangedEvent event) {
+		// Do nothing
+		System.out.println("Inside afterVariableChanged event");
+	}
+
+}
